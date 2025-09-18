2644edcccdc2edc9a342b7e337fa152cc7794760
Gabriel Ruiz <usuariogi@Gabriel-Ruiz.local>
2025-09-17 19:49:12 -0700

    agregando elementos

---
+++ 2.- Practica/Ejercicio 2/ejercicioDos.c
@@ -0,0 +1,28 @@
+/* 
+
+Empleando búsqueda a ciegas y búsqueda informada 
+(A*) crea un programa que resuelva el problema de 15-puzzle. 
+
+*/
+
+#include <studio.h>
+
+// Prototipo de las funciones:
+void algoritmo(){
+
+}
+
+
+int main(){
+
+    // Llamndo a las funciones: 
+    algoritmo();
+    return 0;
+}
+
+// Deficnion de las funciones: 
+void algoritmo(){
+
+}
+
+
