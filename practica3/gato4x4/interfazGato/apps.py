cb696c1df871209070b0d7e131f1f88b38333b19
Rebeca Hernandez Simon <119805635+apofis-rhs@users.noreply.github.com>
2025-10-02 23:38:59 -0600

    Delete practica3/gato4x4 directory

--- practica3/gato4x4/interfazGato/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class InterfazgatoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'interfazGato'
--- practica3/gato4x4/interfazGato/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader 
-
-def gato(request):
-    template = loader.get_template('interfaz.html')
-    return HttpResponse(template.render())
